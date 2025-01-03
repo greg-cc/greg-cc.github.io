5b08ba0c74f2c7b9d9d386e33cade4775d56a3d5
Greg-cc <73477392+greg-cc@users.noreply.github.com>
2025-01-02 23:41:58 -0800

    Create surfcamera.bat

---
+++ surfcamera.bat
@@ -0,0 +1,76 @@
+TOP:
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 12 -i https://cams.cdn-surfline.com/cdn-wc/wc-haggertys/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-dockweiler/playlist.m3u8 
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-dockweiler/playlist.m3u8 
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 12 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeachnorth/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-wedge/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://live.hdontap.com/hls/hosb3/swellmagnet_elporto_strand-ptz.stream/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-andersonst/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-lowers/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-malibufirstpt/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-malibuov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeachnorth/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 12 -i https://cams.cdn-surfline.com/cdn-wc/wc-haggertys/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-andersonstov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://live.hdontap.com/hls/hosb3/swellmagnet_elporto_strand-ptz.stream/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-wedgejetty/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-wedge/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-dockweilernorth/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-grandview/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-dockweiler/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-brucesbeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elporto39th/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-lowers/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-dockweiler/playlist.m3u8 
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elporto41st/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elporto42th/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elporto43th/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-dockweiler/playlist.m3u8 
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elporto44th/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elportoov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-wedge/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-andersonst/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-andersonstov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-malibufirstpt/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-malibuov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrance/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-leocarrillosb/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-brucesbeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-torrancebeach/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-bolsachica/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-sapphire/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-knobhill/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elporto/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elportoclose/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-hbpierssov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-elportofixed/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-surfside/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-sealbeachss/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-topangaoverview/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-hbpiernsov/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-seventeenthst/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-goldenwest/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-bolsachica/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-surfside/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-sealbeachss/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-sealbeachjetty/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-k38/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-lowers/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-swamis/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-osideharbornjetty/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-int/pf-teahupoo/playlist.m3u8
+D:\Portableapps\ffmpeg\bin\ffplay.exe -hwaccel cuda -x 1920 -y 1090 -top 18 -i https://cams.cdn-surfline.com/cdn-wc/wc-leocarrillosb/playlist.m3u8
+
+GOTO TOP:
+ffplay.exe  scale=w=480:h=-1  
+-window_title <window title>  set window title
+-left <x pos>       set the x position for the left of the window
+-top <y pos>        set the y position for the top of the window
+-exitonkeydown <>   exit on key down
+-exitonmousedown <>  exit on mouse down
+-t <duration>       play  "duration" seconds of audio/video
+-vf crop=512:256:2:260,scale=-1:1080:flags=neighbor ??
